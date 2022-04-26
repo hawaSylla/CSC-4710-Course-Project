aa9ae3c2640cad5f2b8301bf4f4f62a484f50063
Hawa S <hmsylla10@gmail.com>
2022-04-25 21:21:12 -0400

    base fighter update

--- insert.sql
+++ insert.sql
@@ -1,5 +1,15 @@
 INSERT INTO Base_Fighter(Fighter_ID,Color_Variant,Smash_Title_of_Origin)
-			VALUES(001,1,"Super Smash Bros.");
+			VALUES(000,1,"Super Smash Bros."),/*Mario */
+				  (001,1,"Super Smash Bros."),/*Luigi */
+				  (002,1,"Super Smash Bros. Melee"),/*Peach */
+                  (003,1,"Super Smash Bros. Melee"),/*Bowser */
+                  (004,1,"Super Smash Bros."),/*Yoshi */
+                  (005,1,"Super Smash Bros."),/*Donkey Kong */
+                  (006,1,"Super Smash Bros."),/*Link */
+                  (007,1,"Super Smash Bros. Melee"),/*Zelda/Sheik */
+                  (008,1,"Super Smash Bros."),/*Samus */
+                  (009,1,"Super Smash Bros."),/*Kirby */
+                  (010,1,"Super Smash Bros.");/*Fox */
 INSERT INTO Base_Music(Music_ID,Smash_Title_of_Origin)
 			VALUES(0001,"Super Smash Bros.");
 INSERT INTO Base_Stage(Stage_ID,Smash_Title_of_Origin)
@@ -9,23 +19,12 @@ INSERT INTO DLC_Fighter(Fighter_ID,Color_Variant,Challenger_Pack_Number,Date_Add
 			VALUES(0001,01,01,01012018);
 INSERT INTO DLC_Music(Music_ID,Challenger_Pack_Number,Date_Added)
 			VALUES(0001,01,01,01012018);
-INSERT INTO Base_Fighter(Fighter_ID,Color_Variant,Smash_Title_of_Origin)
-			VALUES(001,1,"Super Smash Bros.");
-INSERT INTO Base_Music(Music_ID,Smash_Title_of_Origin)
-			VALUES(0001,"Super Smash Bros.");
-INSERT INTO Base_Stage(Stage_ID,Smash_Title_of_Origin)
-			VALUES(0001,"Super Smash Bros.");
-INSERT INTO Base_Stage(Stage_ID,Smash_Title_of_Origin)
-			VALUES(0001,"Super Smash Bros.");
-INSERT INTO Base_Stage(Stage_ID,Smash_Title_of_Origin)
-			VALUES(0001,"Super Smash Bros.");
-INSERT INTO Base_Fighter(Fighter_ID,Color_Variant,Smash_Title_of_Origin)
-			VALUES(001,1,"Super Smash Bros.");
-INSERT INTO Base_Music(Music_ID,Smash_Title_of_Origin)
-			VALUES(0001,"Super Smash Bros.");
-INSERT INTO Base_Stage(Stage_ID,Smash_Title_of_Origin)
-			VALUES(0001,"Super Smash Bros.");
-INSERT INTO Base_Stage(Stage_ID,Smash_Title_of_Origin)
-			VALUES(0001,"Super Smash Bros.");
-INSERT INTO Base_Stage(Stage_ID,Smash_Title_of_Origin)
-			VALUES(0001,"Super Smash Bros.");
+
+INSERT INTO Game_Mode (Mode_Name, Tagline)
+	VALUES 
+		('Classic', 'Each fighter has a route. Try to make it through!'),
+		('Smash','Battle with up to eight players.'),
+		('Spirit Board', 'Unlock DLC spirits on a special DLC fighter Spirit Board!'),
+		('Training', 'Improve your skills by training!'),
+		('World of Light', 'Rescue fighters and spirits in a world where all seems lost!');
+
