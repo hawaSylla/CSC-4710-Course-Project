6b845893e4db590a6a7e860d2cacba3e9efe429a
Hawa S <hmsylla10@gmail.com>
2022-04-25 20:47:53 -0400

    updated insert

---
+++ insert.sql
@@ -0,0 +1,31 @@
+INSERT INTO Base_Fighter(Fighter_ID,Color_Variant,Smash_Title_of_Origin)
+			VALUES(001,1,"Super Smash Bros.");
+INSERT INTO Base_Music(Music_ID,Smash_Title_of_Origin)
+			VALUES(0001,"Super Smash Bros.");
+INSERT INTO Base_Stage(Stage_ID,Smash_Title_of_Origin)
+			VALUES(0001,"Super Smash Bros.");
+            
+INSERT INTO DLC_Fighter(Fighter_ID,Color_Variant,Challenger_Pack_Number,Date_Added)
+			VALUES(0001,01,01,01012018);
+INSERT INTO DLC_Music(Music_ID,Challenger_Pack_Number,Date_Added)
+			VALUES(0001,01,01,01012018);
+INSERT INTO Base_Fighter(Fighter_ID,Color_Variant,Smash_Title_of_Origin)
+			VALUES(001,1,"Super Smash Bros.");
+INSERT INTO Base_Music(Music_ID,Smash_Title_of_Origin)
+			VALUES(0001,"Super Smash Bros.");
+INSERT INTO Base_Stage(Stage_ID,Smash_Title_of_Origin)
+			VALUES(0001,"Super Smash Bros.");
+INSERT INTO Base_Stage(Stage_ID,Smash_Title_of_Origin)
+			VALUES(0001,"Super Smash Bros.");
+INSERT INTO Base_Stage(Stage_ID,Smash_Title_of_Origin)
+			VALUES(0001,"Super Smash Bros.");
+INSERT INTO Base_Fighter(Fighter_ID,Color_Variant,Smash_Title_of_Origin)
+			VALUES(001,1,"Super Smash Bros.");
+INSERT INTO Base_Music(Music_ID,Smash_Title_of_Origin)
+			VALUES(0001,"Super Smash Bros.");
+INSERT INTO Base_Stage(Stage_ID,Smash_Title_of_Origin)
+			VALUES(0001,"Super Smash Bros.");
+INSERT INTO Base_Stage(Stage_ID,Smash_Title_of_Origin)
+			VALUES(0001,"Super Smash Bros.");
+INSERT INTO Base_Stage(Stage_ID,Smash_Title_of_Origin)
+			VALUES(0001,"Super Smash Bros.");
